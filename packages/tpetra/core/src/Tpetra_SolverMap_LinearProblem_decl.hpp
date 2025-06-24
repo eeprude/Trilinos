bf134ca900cf2be70156a4e2b6118162c7a81b94
Ernesto Prudencio <eeprude@sandia.gov>
2025-06-24 09:16:39 -0600

    Backup

---
+++ packages/tpetra/core/src/Tpetra_SolverMap_LinearProblem_decl.hpp
@@ -0,0 +1,54 @@
+// @HEADER
+// *****************************************************************************
+//          Tpetra: Templated Linear Algebra Services Package
+//
+// Copyright 2008 NTESS and the Tpetra contributors.
+// SPDX-License-Identifier: BSD-3-Clause
+// *****************************************************************************
+// @HEADER
+
+#ifndef TPETRA_SOLVERMAP_LINEARPROBLEM_DECL_HPP
+#define TPETRA_SOLVERMAP_LINEARPROBLEM_DECL_HPP
+
+/// \file Tpetra_SolverMap_LinearProblem_decl.hpp
+/// \brief Declaration of the Tpetra::LinearProblem_SolverMap class
+
+#include <Tpetra_Transform.h>
+
+//#include <Tpetra_SolverMap_CrsMatrix.h> // AquiEEP
+
+namespace Tpetra {
+
+///
+/** Constructs a LinearProblem with a "fixed" Column Map for the CrsMatrix.
+ *  Almost entirely a view except for the "fixed" Tpetra_CrsGraph.
+ */
+template <class Scalar,
+          class LocalOrdinal,
+          class GlobalOrdinal,
+          class Node>
+class LinearProblem_SolverMap : public StructuralSameTypeTransform< LinearProblem<Scalar, LocalOrdinal, GlobalOrdinal, Node> >
+{
+  //CrsMatrix_SolverMap<Scalar, LocalOrdinal, GlobalOrdinal, Node> crsMatrixSolverMapTrans_; // AquiEEP
+
+public:
+
+  ///
+  /** Constructor
+   */
+  LinearProblem_SolverMap();
+
+  ///
+  /** Destructor
+   */
+  ~LinearProblem_SolverMap();
+
+  ///
+  /** Constructs "fixed" Tpetra::LinearProblem
+   */
+  NewTypeRef operator()( OriginalTypeRef orig );
+};
+
+} //namespace Tpetra
+
+#endif // TPETRA_SOLVERMAP_LINEARPROBLEM_DECL_HPP
---
+++ packages/tpetra/core/src/Tpetra_SolverMap_LinearProblem_def.hpp
@@ -0,0 +1,77 @@
+// @HEADER
+// *****************************************************************************
+//          Tpetra: Templated Linear Algebra Services Package
+//
+// Copyright 2008 NTESS and the Tpetra contributors.
+// SPDX-License-Identifier: BSD-3-Clause
+// *****************************************************************************
+// @HEADER
+
+#ifndef TPETRA_SOLVERMAP_LINEARPROBLEM_DEF_HPP
+#define TPETRA_SOLVERMAP_LINEARPROBLEM_DEF_HPP
+
+/// \file Tpetra_SolverMap_LinearProblem_def.hpp
+/// \brief Definition of the Tpetra::LinearProblem_SolverMap class
+///
+/// If you want to use Tpetra::LinearProblem_SolverMap, include
+/// "Tpetra_SolverMap_LinearProblem.hpp", a file which CMake generates
+/// and installs for you).
+///
+/// If you only want the declaration of Tpetra::LinearProblem_SolverMap,
+/// include "Tpetra_SolverMap_LinearProblem_decl.hpp".
+
+#include <Tpetra_LinearProblem.h>
+#include <Tpetra_CrsMatrix.h>
+
+namespace Tpetra {
+
+template <class Scalar,
+          class LocalOrdinal,
+          class GlobalOrdinal,
+          class Node>
+LinearProblem_SolverMap<Scalar, LocalOrdinal, GlobalOrdinal, Node>::LinearProblem_SolverMap()
+{
+  // Nothing to do
+}
+
+template <class Scalar,
+          class LocalOrdinal,
+          class GlobalOrdinal,
+          class Node>
+LinearProblem_SolverMap<Scalar, LocalOrdinal, GlobalOrdinal, Node>::~LinearProblem_SolverMap()
+{
+  if ((newObj_ != nullptr ) &&
+      (newObj_ != origObj_)) {
+    delete newObj_;
+  }
+}
+
+template <class Scalar,
+          class LocalOrdinal,
+          class GlobalOrdinal,
+          class Node>
+typename LinearProblem_SolverMap<Scalar, LocalOrdinal, GlobalOrdinal, Node>::NewTypeRef
+LinearProblem_SolverMap<Scalar, LocalOrdinal, GlobalOrdinal, Node>::
+operator()( typename LinearProblem_SolverMap<Scalar, LocalOrdinal, GlobalOrdinal, Node>::OriginalTypeRef orig )
+{
+  origObj_ = &orig;
+
+  CrsMatrix<Scalar, LocalOrdinal, GlobalOrdinal, Node> * OldMatrix = dynamic_cast< CrsMatrix<Scalar, LocalOrdinal, GlobalOrdinal, Node>* >( orig.GetMatrix() );
+  MultiVector<Scalar, LocalOrdinal, GlobalOrdinal, Node> * OldRHS = orig.GetRHS();
+  MultiVector<Scalar, LocalOrdinal, GlobalOrdinal, Node> * OldLHS = orig.GetLHS();
+
+  CrsMatrix<Scalar, LocalOrdinal, GlobalOrdinal, Node> & NewMatrix = nullptr; // crsMatrixSolverMapTrans_( *OldMatrix ); // AquiEEP
+
+  if( &NewMatrix == OldMatrix ) //same matrix so use same problem
+    newObj_ = origObj_;
+  else
+    newObj_ = new LinearProblem<Scalar, LocalOrdinal, GlobalOrdinal, Node>( &NewMatrix, OldLHS, OldRHS );
+
+  return *newObj_;
+}
+
+} //namespace Tpetra
+
+#endif // TPETRA_SOLVERMAP_LINEARPROBLEM_DEF_HPP
+
+
